f0142669cd9d9288d552430b724681d8809a3dc9
Xiaoyu Luo <xluoforhire@gmail.com>
2019-04-06 18:23:18 -0400

    install gh-pages / deply script / homepage script

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import 'tachyons';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
-ReactDOM.render( 
-    <App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
